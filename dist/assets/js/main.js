36d0eb07549543ce18ea0ce3956fc2b6c22b1696
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-04-07 22:14:06 +0600

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,4 +1,23 @@
 // header toggle
 $("#dropdown").click(() => {
     $("#dropdownArea").toggle();
-})
+});
+
+// owl carousel
+$('.banner-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
