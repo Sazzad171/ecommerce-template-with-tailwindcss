d48bfaa108398b071370db8d6d826770b9b29302
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-04-25 22:13:02 +0600

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -21,3 +21,22 @@ $('.banner-slider').owlCarousel({
         }
     }
 });
+// flash deal carousel
+$('.flash-deal-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:2,
+            margin:10
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
